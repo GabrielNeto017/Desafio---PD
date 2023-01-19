eb4ea4c4b80907df3b8605b319818d5d983d2813
Gabriel Neto <gabriel_nikel@hotmail.com>
2023-01-19 12:59:55 -0300

    Desafio

---
+++ lib/GPIO/GPIO.h
@@ -0,0 +1,21 @@
+/*==================================
+
+Author: Gabriel Luiz B. Neto
+Date:19/01/2023
+Project: Desafio
+
+==================================*/
+
+#ifndef _GPIO_H
+#define _GPIO_H
+
+
+#include <Arduino.h>
+#include <PinDefinition.h>
+
+//Declaração das funções
+void GPIO_Init();
+void botaoacionado();
+
+
+#endif
---
+++ lib/Serial/Serial.h
@@ -0,0 +1,21 @@
+/*==================================
+
+Author: Gabriel Luiz B. Neto
+Date: 19/01/2022
+Project: Desafio
+
+==================================*/
+
+#ifndef _SERIAL_H
+#define _SERIAL_H
+
+
+#include <Arduino.h>
+#include <PinDefinition.h>
+
+//Declaração das Variáveis
+
+void Serial_Init();
+
+
+#endif
